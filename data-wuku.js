e353f3817722bbfa7e5d5e4730a0fb8f0cea5454
tius75 <76941855+tius75@users.noreply.github.com>
2026-01-28 21:23:44 +0700

    Update data-wuku.js

--- data-wuku.js
+++ data-wuku.js
@@ -1,10 +1,6 @@
 // data-wuku.js
 const DATA_WUKU = {
-    "Sinta": `<b>WUKU SINTA - RINGKEL</b><br>
-        Dewa Bumi: Bethara Yamadipati.<br>
-        Pohonnya Gendhayakan: Pelindung orang sakit.<br>
-        Burungnya Gagak: Bisa menerima ilham.<br>
-        Aralnya: Ketika sukses tidak dermawan menyebabkan jatuhnya.`,
+    "Sinta": `Wuku SINTA: RINGKEL<br><br>Hari Naas Kamis Senen<br>Dewa Bumi: Bethara Yamadipati.Pohonnya Gendhayakan : Menjadi pelindung atau penolong orang sakit.<br>Burungnya Gagak: Bisa menerima wangsit /ilham..<br>• Gedhongnya di depan : Suka memperlihatkankekayaannya..<br><br>• Amandhi Umbul-umbul: Punya kemuliaan..<br>• Wuku Sinta Wulan Karahinan (bulan kesiangan) : Tidak sabaran..<br><br>• Aralnya Ketika sedang mendapat peningkatan tidak dermawan, maka menyebabkan kejatuhannya..<br>Sedekah / sesaji Nasi pulen dang-dangan beras senilai zakat fitrah, lauknya pindang kerbau seharga 21 ketheng tidak boleh menawar..<br>• Do'anya Tolak bilahi..<br>• Kala Jaya Bumi : Ada di timur laut menghadap barat daya..<br><br>• Saat wukunya berjalan, selama 7 hari: Sebaiknya tidak.bepergian ke arah timur laut..<br>• Sinta patine wong ngawig Matinya orang yang merasa mulia..<br>• Wuku Sinta baik untuk mengobati, membuat sarat supaya.banyak hujan, mengobati menawarkan orang terkena pengasihan atau sebaliknya..<br>• Tidak baik untuk menanam dan membuka pekarangan.
     
     "Landep": `<b>WUKU LANDEP - SONYA</b><br>
         Dewa Bumi: Bethara Maha Dewa.<br>
