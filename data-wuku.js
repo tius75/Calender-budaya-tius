c28187da0973672adc0bea6d64a3cc16b0975459
tius75 <76941855+tius75@users.noreply.github.com>
2026-01-28 21:32:34 +0700

    Update data-wuku.js

--- data-wuku.js
+++ data-wuku.js
@@ -1,12 +1,27 @@
 // data-wuku.js
 const DATA_WUKU = {
-    "Sinta": `Wuku SINTA: RINGKEL<br><br>Hari Naas Kamis Senen<br>Dewa Bumi: Bethara Yamadipati.Pohonnya Gendhayakan : Menjadi pelindung atau penolong orang sakit.<br>Burungnya Gagak: Bisa menerima wangsit /ilham..<br>• Gedhongnya di depan : Suka memperlihatkankekayaannya..<br><br>• Amandhi Umbul-umbul: Punya kemuliaan..<br>• Wuku Sinta Wulan Karahinan (bulan kesiangan) : Tidak sabaran..<br><br>• Aralnya Ketika sedang mendapat peningkatan tidak dermawan, maka menyebabkan kejatuhannya..<br>Sedekah / sesaji Nasi pulen dang-dangan beras senilai zakat fitrah, lauknya pindang kerbau seharga 21 ketheng tidak boleh menawar..<br>• Do'anya Tolak bilahi..<br>• Kala Jaya Bumi : Ada di timur laut menghadap barat daya..<br><br>• Saat wukunya berjalan, selama 7 hari: Sebaiknya tidak.bepergian ke arah timur laut..<br>• Sinta patine wong ngawig Matinya orang yang merasa mulia..<br>• Wuku Sinta baik untuk mengobati, membuat sarat supaya.banyak hujan, mengobati menawarkan orang terkena pengasihan atau sebaliknya..<br>• Tidak baik untuk menanam dan membuka pekarangan.
-    
+    "Sinta": `Wuku SINTA: RINGKEL<br><br>
+        Hari Naas Kamis Senen<br>
+        Dewa Bumi: Bethara Yamadipati.<br>
+        Pohonnya Gendhayakan: Menjadi pelindung atau penolong orang sakit.<br>
+        Burungnya Gagak: Bisa menerima wangsit /ilham.<br>
+        • Gedhongnya di depan: Suka memperlihatkan kekayaannya.<br><br>
+        • Amandhi Umbul-umbul: Punya kemuliaan.<br>
+        • Wuku Sinta Wulan Karahinan (bulan kesiangan): Tidak sabaran.<br><br>
+        • Aralnya Ketika sedang mendapat peningkatan tidak dermawan, maka menyebabkan kejatuhannya.<br>
+        Sedekah / sesaji Nasi pulen dang-dangan beras senilai zakat fitrah, lauknya pindang kerbau seharga 21 ketheng tidak boleh menawar.<br>
+        • Do'anya Tolak bilahi.<br>
+        • Kala Jaya Bumi: Ada di timur laut menghadap barat daya.<br><br>
+        • Saat wukunya berjalan, selama 7 hari: Sebaiknya tidak bepergian ke arah timur laut.<br>
+        • Sinta patine wong ngawig Matinya orang yang merasa mulia.<br>
+        • Wuku Sinta baik untuk mengobati, membuat sarat supaya banyak hujan, mengobati menawarkan orang terkena pengasihan atau sebaliknya.<br>
+        • Tidak baik untuk menanam dan membuka pekarangan.`, // Tanda penutup backtick dan koma ada di sini
+
     "Landep": `<b>WUKU LANDEP - SONYA</b><br>
         Dewa Bumi: Bethara Maha Dewa.<br>
         Burungnya Atat Kembang: Sering mengabdi pada orang besar.<br>
         Aralnya: Kejatuhan pohon/kayu.`,
-    
+
     "Wukir": `<b>WUKU WUKIR - DONYO</b><br>
         Dewa Bumi: Bethara Mahayekti.<br>
         Pohonnya Nagasari: Wataknya prihatin.<br>
