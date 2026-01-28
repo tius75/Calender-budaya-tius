d533b5426d2ca44832624afc2f7dd78ed3146f21
tius75 <76941855+tius75@users.noreply.github.com>
2026-01-28 13:30:40 +0700

    Update data-hari.js

--- data-hari.js
+++ data-hari.js
@@ -1,27 +1,8 @@
-// Simpan sebagai data-srijati.js
-// JANGAN gunakan kata 'export'
-Dina:Rebo
-Pendiam, pemomong dan penyabar.Pasaran:Legi
-Bertanggung jawab, murah hati, enak dalam pergaulan, selalu gembira seperti tidak pernah susah, sering kena fitnah, kuat tidak tidur malam hari, berhati-hati namun sering bingung sendiri, bicaranya berisi. Banyak keberuntungan dan kesialannya.Haståwårå/Padewan:Brama
-Tidak sabaran, emosional.Sadwårå:Paningron
-(Ikan) Kena tipu.Sångåwårå/Padangon:Tulus
-(Air) Tekun, dermawan, dikagumi, lemah lembut.Saptåwårå/Pancasuda:Sumur Sinaba
-Luas wawasan, bisa menjadi sumber orang mencari ilmu.Rakam:Dêmang Kadhuruwan
-Sering mendapat perkara, suka membantah.Paarasan:Aras Kêmbang
-Memiliki pesona daya tarik terhadap lawan jenis.
-Watak berdasarkan wuku
-Wuku : Tambir
-• Dewa Bumi : Bethara Siwah, kebaikan lahirnya bermuatan pamrih di batin.
-• Pohonnya Upas: Tidak bisa dijadikan tempat berlindung / mengabdi.
-• Burungnya Prenjak : tinggi cita-citanya.
-• Menyandhing Gedhong : suka membual.
-• Tambir Anggara Kasih upas racun : Batinnya tidak selamat.
-• Aralnya : dijahili / dikerjai orang.
-• Sedekah / sesaji : Nasi pulen dan nasi uduk, lauknya pindang bebek dan ayam.
-• Do'anya : slamet pina, slawatnya : pisau baja dan jarum satu.
-• Kala Jaya Bumi : ada di barat daya menghadap ke timur laut.
-• Saat wukunya berjalan selama 7 hari, sebaiknya menghindari bepergian yang menuju arah ke barat daya.
-• Tambir lesu sarirane : sering kehilangan gairah / semangat.
-• Wuku Tambir baik untuk pergi mencari nafkah, menanam pohon buah-buahan (palakina), menancapkan turus (cabang / ranting pohon yang bisa tumbuh setelah ditancapkan di tanah), berguru ilmu kebatinan dan pergi berperang
-
-  
+// data-hari.js
+const DATA_HARI = {
+    "Rabu Legi": `<b>Watak Rabu Legi:</b><br>
+        Bertanggung jawab, murah hati, enak dalam pergaulan, selalu gembira, luas wawasan (Sumur Sinaba), memiliki pesona daya tarik tinggi (Aras Kembang).`,
+    
+    "Minggu Pahing": `<b>Watak Minggu Pahing:</b><br>
+        Giat bekerja, mandiri, dan sangat menjaga kehormatan keluarga.`
+};
