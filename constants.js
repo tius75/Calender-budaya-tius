46c83bb80455eb79497c81e9c4ccd59b5bab03ed
tius75 <76941855+tius75@users.noreply.github.com>
2026-01-28 12:07:24 +0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,22 +1,5 @@
-// Tabel Data Neptu Jawa
-export const NEPTU_HARI = {
-    'Minggu': 5, 'Senin': 4, 'Selasa': 3, 'Rabu': 7, 
-    'Kamis': 8, 'Jumat': 6, 'Sabtu': 9
-};
+export const HARI = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+export const PASARAN = ['Legi', 'Pahing', 'Pon', 'Wage', 'Kliwon'];
 
-export const NEPTU_PASARAN = {
-    'Legi': 5, 'Pahing': 9, 'Pon': 7, 'Wage': 4, 'Kliwon': 8
-};
-
-// Data Ramalan Tarot Singkat
-export const TAROT_DATA = [
-    "The Fool: Awal baru, petualangan.",
-    "The Magician: Kemauan kuat, aksi nyata.",
-    "The High Priestess: Intuisi, rahasia batin.",
-    // ... tambahkan sisa data dari array asli
-];
-
-// Data Wuku dan Elemen
-export const WUKU_LIST = ["Sinta", "Landep", "Wukir", "Kurantil", "Tolu", "Gumbreg", /* dst */];
-
-
+export const NEPTU_HARI = { 'Minggu': 5, 'Senin': 4, 'Selasa': 3, 'Rabu': 7, 'Kamis': 8, 'Jumat': 6, 'Sabtu': 9 };
+export const NEPTU_PASARAN = { 'Legi': 5, 'Pahing': 9, 'Pon': 7, 'Wage': 4, 'Kliwon': 8 };
