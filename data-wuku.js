29019e41286f308938e6020b83d8bd737ecfb8b5
tius75 <76941855+tius75@users.noreply.github.com>
2026-01-28 21:43:14 +0700

    Update data-wuku.js

--- data-wuku.js
+++ data-wuku.js
@@ -1,29 +1,197 @@
 // data-wuku.js
 const DATA_WUKU = {
-    "Sinta": `Wuku SINTA: RINGKEL<br><br>
-        Hari Naas Kamis Senen<br>
-        Dewa Bumi: Bethara Yamadipati.<br>
-        Pohonnya Gendhayakan: Menjadi pelindung atau penolong orang sakit.<br>
-        Burungnya Gagak: Bisa menerima wangsit /ilham.<br>
-        • Gedhongnya di depan: Suka memperlihatkan kekayaannya.<br><br>
-        • Amandhi Umbul-umbul: Punya kemuliaan.<br>
-        • Wuku Sinta Wulan Karahinan (bulan kesiangan): Tidak sabaran.<br><br>
-        • Aralnya Ketika sedang mendapat peningkatan tidak dermawan, maka menyebabkan kejatuhannya.<br>
-        Sedekah / sesaji Nasi pulen dang-dangan beras senilai zakat fitrah, lauknya pindang kerbau seharga 21 ketheng tidak boleh menawar.<br>
-        • Do'anya Tolak bilahi.<br>
-        • Kala Jaya Bumi: Ada di timur laut menghadap barat daya.<br><br>
-        • Saat wukunya berjalan, selama 7 hari: Sebaiknya tidak bepergian ke arah timur laut.<br>
-        • Sinta patine wong ngawig Matinya orang yang merasa mulia.<br>
-        • Wuku Sinta baik untuk mengobati, membuat sarat supaya banyak hujan, mengobati menawarkan orang terkena pengasihan atau sebaliknya.<br>
-        • Tidak baik untuk menanam dan membuka pekarangan.`, // Tanda penutup backtick dan koma ada di sini
+    "Sinta": `<b>Wuku SINTA: RINGKEL</b><br><br>
+        Hari Naas: Kamis, Senin<br>
+        • Dewa Bumi: Bethara Yamadipati.<br>
+        • Pohon: Gendhayakan (Pelindung/penolong orang sakit).<br>
+        • Burung: Gagak (Menerima wangsit/ilham).<br>
+        • Gedhong: Di depan (Suka memperlihatkan kekayaan).<br>
+        • Sifat: Wulan Karahinan (Bulan kesiangan), tidak sabaran.<br>
+        • Aral: Jatuh jika tidak dermawan saat sukses.<br>
+        • Kala Jaya Bumi: Timur Laut menghadap Barat Daya.<br>
+        • Baik untuk: Pengobatan, sarat hujan, menawarkan pengasihan.<br>
+        • Tidak baik untuk: Menanam dan membuka pekarangan.`,
 
-    "Landep": `<b>WUKU LANDEP - SONYA</b><br>
-        Dewa Bumi: Bethara Maha Dewa.<br>
-        Burungnya Atat Kembang: Sering mengabdi pada orang besar.<br>
-        Aralnya: Kejatuhan pohon/kayu.`,
+    "Landep": `<b>Wuku LANDEP: SONYA</b><br><br>
+        Hari Naas: Rabu, Sabtu, Minggu<br>
+        • Dewa Bumi: Bethara Maha Dewa.<br>
+        • Pohon: Gendhayakan (Pelindung/penolong orang sakit).<br>
+        • Burung: Atat Kembang (Mengabdi pada orang besar).<br>
+        • Sifat: Soroting Srengenge (Menerangi hati semua orang). Perintah lembut di depan, panas di belakang.<br>
+        • Aral: Kejatuhan pohon/kayu.<br>
+        • Kala Jaya Bumi: Barat menghadap ke Timur.<br>
+        • Baik untuk: Mengasah pedang, membuat pagar, membuat wisaya ikan.<br>
+        • Tidak baik untuk: Pindah rumah, perkawinan, membuat pintu.`,
 
-    "Wukir": `<b>WUKU WUKIR - DONYO</b><br>
-        Dewa Bumi: Bethara Mahayekti.<br>
-        Pohonnya Nagasari: Wataknya prihatin.<br>
-        Aralnya: Dianiaya.`
+    "Wukir": `<b>Wuku WUKIR: DONYO</b><br><br>
+        Hari Naas: Selasa, Jumat<br>
+        • Dewa Bumi: Bethara Mahayekti.<br>
+        • Pohon: Nagasari (Wataknya prihatin).<br>
+        • Burung: Manyar (Tidak mau dilebihi).<br>
+        • Sifat: Gunung (Indah dari jauh, berbahaya dari dekat), sulit diketahui isi hatinya.<br>
+        • Aral: Dianiaya.<br>
+        • Kala Jaya Bumi: Tenggara menghadap Barat Laut.<br>
+        • Baik untuk: Mantu, memperbaiki barang, berteman tulus.<br>
+        • Tidak baik untuk: Tetirah, mengobati penyakit, mendirikan rumah.`,
+
+    "Kurantil": `<b>Wuku KURANTIL: MALIHAN</b><br><br>
+        Hari Naas: Senin, Kamis<br>
+        • Dewa Bumi: Bethara Langsur.<br>
+        • Pohon: Inggas (Terburu-buru tapi penyabar).<br>
+        • Burung: Slindhitan (Tidak suka menganggur).<br>
+        • Sifat: Boros, tidak bisa menyimpan harta, suka selingkuh.<br>
+        • Aral: Saat memanjat.<br>
+        • Kala Jaya Bumi: Di bawah menghadap ke Atas.<br>
+        • Baik untuk: Mencari jodoh.<br>
+        • Tidak baik untuk: Menikahkan anak, menanam, berteman.`,
+
+    "Tolu": `<b>Wuku TOLU: SONYA</b><br><br>
+        Hari Naas: Rabu, Sabtu, Minggu<br>
+        • Dewa Bumi: Bethara Bayu.<br>
+        • Burung: Branjangan (Suka membuat perkara).<br>
+        • Sifat: Angkuh, susah dilayani kemauannya. Keberuntungan jatuh belakangan.<br>
+        • Aral: Gigitan taring dan sengat.<br>
+        • Kala Jaya Bumi: Barat Laut menghadap Tenggara.<br>
+        • Baik untuk: Mencari nafkah, mengobati orang, menanam, mantu.<br>
+        • Tidak baik untuk: Berkhianat, berjudi, memanen buah tinggi.`,
+
+    "Gumbreg": `<b>Wuku GUMBREG: NYAWA</b><br><br>
+        Hari Naas: Selasa, Jumat<br>
+        • Dewa Bumi: Bethara Cakra.<br>
+        • Pohon: Beringin (Tempat berlindung).<br>
+        • Burung: Ayam Alas (Disenangi orang berpangkat).<br>
+        • Sifat: Berwibawa, perintah diikuti bawahan, ikhlas.<br>
+        • Aral: Saat berada di air.<br>
+        • Kala Jaya Bumi: Di Selatan.<br>
+        • Baik untuk: Berbesanan, mencari nafkah/keberuntungan.<br>
+        • Tidak baik untuk: Menanam kebun, mendirikan rumah.`,
+
+    "Warigalit": `<b>Wuku WARIGALIT: RINGKEL</b><br><br>
+        Hari Naas: Senin, Kamis<br>
+        • Dewa Bumi: Bethara Asmara.<br>
+        • Pohon: Sulkastri (Kesayangan pembesar).<br>
+        • Burung: Kepodang (Wataknya pemarah).<br>
+        • Sifat: Selalu prihatin, sering kekurangan sandang pangan.<br>
+        • Aral: Terserempet perkara.<br>
+        • Kala Jaya Bumi: Di Atas menghadap ke Bawah.<br>
+        • Baik untuk: Menjalin persaudaraan, memuliakan leluhur, mengunjungi kerabat.<br>
+        • Tidak baik untuk: Menyeleweng, berperang.`,
+
+    "Wariagung": `<b>Wuku WARIAGUNG: SONYA</b><br><br>
+        Hari Naas: Rabu, Sabtu, Minggu<br>
+        • Dewa Bumi: Bethara Maharesi.<br>
+        • Pohon: Cemara (Angkuh, suka menggoda).<br>
+        • Burung: Bethet (Bisa mencari nafkah sendiri).<br>
+        • Sifat: Sangat memperhatikan sumber penghidupan, hemat.<br>
+        • Aral: Berasal dari sanak saudara sendiri.<br>
+        • Kala Jaya Bumi: Utara menghadap ke Selatan.<br>
+        • Baik untuk: Mendirikan rumah, bercocok tanam, berbesanan.<br>
+        • Tidak baik untuk: Pindah tempat, menyiksa binatang.`,
+
+    "Julungwangi": `<b>Wuku JULUNGWANGI: SONYA</b><br><br>
+        Hari Naas: Selasa, Jumat<br>
+        • Dewa Bumi: Bethara Sambo.<br>
+        • Pohon: Cempaka (Banyak disenangi orang).<br>
+        • Burung: Kutilang (Banyak bicaranya).<br>
+        • Sifat: Ikhlas, berbudi baik, tidak suka simpan harta (boros).<br>
+        • Aral: Diterkam harimau.<br>
+        • Kala Jaya Bumi: Barat Daya menghadap Timur Laut.<br>
+        • Baik untuk: Lakubrata, mencari wahyu, membuka tanah menanam.<br>
+        • Tidak baik untuk: Pindah tempat, hajat, mendirikan bangunan.`,
+
+    "Sungsang": `<b>Wuku SUNGSANG: MALIHAN</b><br><br>
+        Hari Naas: Senin, Kamis<br>
+        • Dewa Bumi: Bethara Gana.<br>
+        • Pohon: Tangan (Tidak mau menganggur).<br>
+        • Burung: Nori (Boros, jauh rejekinya, hati keras).<br>
+        • Sifat: Mega Mendhung (Tega, berani, berwibawa tapi gerah).<br>
+        • Aral: Terkena besi.<br>
+        • Kala Jaya Bumi: Timur menghadap ke Barat.<br>
+        • Baik untuk: Mencari nafkah, pindah tempat, berbesanan.<br>
+        • Tidak baik untuk: Memanjat, menebang kayu, berperang.`,
+
+    "Galungan": `<b>Wuku GALUNGAN: SONYA</b><br><br>
+        Hari Naas: Rabu, Sabtu, Minggu<br>
+        • Dewa Bumi: Bethara Kamajaya (Pecinta & Setia).<br>
+        • Burung: Bidho (Hati keras, ingin milik orang lain).<br>
+        • Sifat: Dermawan tapi boros, sering terlena keinginan.<br>
+        • Aral: Suka bertengkar.<br>
+        • Kala Jaya Bumi: Timur Laut.<br>
+        • Baik untuk: Tirakat, mengunjungi kerabat, menuntut ilmu.<br>
+        • Tidak baik untuk: Menanam bambu, menikah, mendirikan rumah.`,
+
+    "Kuningan": `<b>Wuku KUNINGAN: NYAWA</b><br><br>
+        Hari Naas: Selasa, Jumat<br>
+        • Dewa Bumi: Bethara Indra.<br>
+        • Pohon: Wijayakusuma (Rupawan).<br>
+        • Burung: Urang-urangan (Kikir).<br>
+        • Sifat: Terpuji, selamat (Pinuteja).<br>
+        • Aral: Diamuk / dikeroyok.<br>
+        • Kala Jaya Bumi: Barat menghadap ke Timur.<br>
+        • Baik untuk: Menjalin persaudaraan, mencari nafkah, menolong orang.<br>
+        • Tidak baik untuk: Menanam, menikahkan anak.`,
+
+    "Langkir": `<b>Wuku LANGKIR: RINGKEL</b><br><br>
+        Hari Naas: Senin, Kamis<br>
+        • Dewa Bumi: Bethara Kala.<br>
+        • Pohon: Cemara & Ingas (Panas, tidak bisa jadi pelindung).<br>
+        • Burung: Gemak (Berani bertindak jahat).<br>
+        • Sifat: Hati kaku menyusahkan diri sendiri, sering musuh.<br>
+        • Aral: Kecurian dan berkelahi.<br>
+        • Kala Jaya Bumi: Tenggara menghadap Barat Laut.<br>
+        • Baik untuk: Menanam, bepergian, mewarangi senjata.<br>
+        • Tidak baik untuk: Berkhianat, bertengkar.`,
+
+    "Mandasiya": `<b>Wuku MANDASIYA: SONYA</b><br><br>
+        Hari Naas: Rabu, Sabtu, Minggu<br>
+        • Dewa Bumi: Bethara Brama.<br>
+        • Burung: Platuk Bawang (Tenaga kuat).<br>
+        • Sifat: Hemat, tinggi hati, ingin disanjung, pelindung orang lain.<br>
+        • Aral: Gigitan taring.<br>
+        • Kala Jaya Bumi: Di bawah menghadap ke Atas.<br>
+        • Baik untuk: Persahabatan, mengobati penyakit, mantu.<br>
+        • Tidak baik untuk: Bepergian jauh, membuat sumur.`,
+
+    "Julungpujut": `<b>Wuku JULUNGPUJUT: DONYA</b><br><br>
+        Hari Naas: Selasa, Jumat<br>
+        • Dewa Bumi: Bethara Guritno.<br>
+        • Pohon: Rembuyut (Penampilan baik).<br>
+        • Burung: Emprit Tondhang (Mandiri, bicara baik).<br>
+        • Sifat: Sembrono, nafsu besar, nuansa keberahian aktif.<br>
+        • Aral: Ditenung / disantet.<br>
+        • Kala Jaya Bumi: Barat Laut menghadap Tenggara.<br>
+        • Baik untuk: Mencari nafkah, pelihara hewan, menanam buah.<br>
+        • Tidak baik untuk: Merencanakan pergi mencari syarat.`,
+
+    "Pahang": `<b>Wuku PAHANG: MALIHAN</b><br><br>
+        Hari Naas: Senin, Kamis<br>
+        • Dewa Bumi: Bethara Tantra.<br>
+        • Burung: Cocak (Pandai bicara).<br>
+        • Sifat: Ikhlas, dermawan, tapi mudah tersinggung.<br>
+        • Aral: Dianiaya.<br>
+        • Kala Jaya Bumi: Selatan menghadap ke Utara.<br>
+        • Baik untuk: Mengobati penyakit, menanam, menikah.<br>
+        • Tidak baik untuk: Bepergian jauh, mencari nafkah.`,
+
+    "Kuruwelut": `<b>Wuku KURUWELUT: SONYA</b><br><br>
+        Hari Naas: Rabu, Sabtu, Minggu<br>
+        • Dewa Bumi: Bethara Wisnu.<br>
+        • Pohon: Parijatha (Cekatan tapi nakal).<br>
+        • Burung: Sepahan (Selalu prihatin).<br>
+        • Sifat: Hati dipenuhi perasaan selamat, fisik cenderung lemah.<br>
+        • Aral: Terkena peluru.<br>
+        • Kala Jaya Bumi: Di Atas menghadap ke Bawah.<br>
+        • Baik untuk: Melihat calon mantu, rencana bangun rumah.<br>
+        • Tidak baik untuk: Mengobati penyakit, menanam jagung.`,
+
+    "Watugunung": `<b>Wuku WATUGUNUNG: SONYA</b><br><br>
+        Hari Naas: Selasa, Jumat<br>
+        • Dewa Bumi: Bethara Anataboga.<br>
+        • Pohon: Wijaya Kusuma (Hidup sebagai pendeta).<br>
+        • Burung: Gogik (Banyak bicara bernada nasehat).<br>
+        • Sifat: Suka menjalani lakubrata, sering kesusahan akibat ulah sendiri.<br>
+        • Aral: Dianiaya.<br>
+        • Kala Jaya Bumi: Timur menghadap ke Barat.<br>
+        • Baik untuk: Mencari penyembuhan, menanam, berteman, berbesanan.<br>
+        • Tidak baik untuk: Membuat pagar pekarangan, simpan harta.`
 };
